2b65ddf65720cad5b16aea2bc66e841693d2bbc8
Pablo Souza <pablovsouza@gmail.com>
2022-08-15 19:34:32 +0100

    remove dist

--- dist/templates/field.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const FIELD_TEMPLATE_DEFAULT = "\t#!{DECORATORS}\n\t#!{NAME}: #!{TYPE} = #!{DEFAULT}\n\t";
-export declare const FIELD_TEMPLATE = "\t#!{DECORATORS}\n\t#!{NAME}: #!{TYPE}\n\t";
